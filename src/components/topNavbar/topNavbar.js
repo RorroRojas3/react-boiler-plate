bc2d5420c456c7cdc5e83fa42c3a0765e085c904
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2021-01-28 17:28:58 -0500

    Added Navbar

---
+++ src/components/topNavbar/topNavbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+const TopNavbar = () => {
+  return (
+    <Navbar bg="dark">
+      <Navbar.Brand href="#home">
+        <img
+          src="/logo.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="React Bootstrap logo"
+        />
+      </Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default TopNavbar;
