9a8a0bfd141106a8d0983040d9388aa993c9526f
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2021-01-28 19:20:54 -0500

    Routing working - Home/About/Contact/NotFound implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import TopNavbar from "./components/topNavbar/topNavbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 
+// My components
+import TopNavbar from "./components/topNavbar/topNavbar";
+import Home from "./pages/home/home";
+import About from "./pages/about/about";
+import Contact from "./pages/contact/contact";
+import NotFound from "./pages/notFound/notFound";
+
 function App() {
   return (
-    <div>
+    <Router>
       <TopNavbar />
-      <Container fluid>adsasdasd</Container>
-    </div>
+      <Container fluid>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
