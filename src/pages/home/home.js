222413f5e4ac1452b4d97513ccdc55563b042208
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2021-01-31 20:34:17 -0500

    demoContext working

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,29 @@
-import { React } from "react";
+import { React, useContext } from "react";
+import { DemoContext } from "../../context/demoContext/demoContext";
+import { Button } from "react-bootstrap";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [count, setCount] = useContext(DemoContext);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <Button variant="success" onClick={increment}>
+        Increment
+      </Button>{" "}
+      <Button variant="danger" onClick={decrement}>
+        Decrement
+      </Button>{" "}
+      {count}
+    </div>
+  );
 };
 
 export default Home;
